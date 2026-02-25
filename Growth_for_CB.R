81eb9e2af18ca800bd8295f3b5cc403ae734ec2a
Thomas Oliver <1505576515113302@mil>
2026-02-25 13:07:06 -1000

    CB and rename

--- Growth_for_CB.R
+++ Growth_for_CB.R
@@ -1,4 +1,51 @@
 library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+CBdb=read.csv("C:/Users/Thomas.Oliver/Downloads/prod_dbase_ha.csv")
+head(CBdb)
+
+DENdb=read.csv("C:/Users/Thomas.Oliver/Downloads/export_20260209/data_20260209.csv")
+head(DENdb)
+
+Nsp=sort(table(DENdb$specie_name))
+Nsp10=names(Nsp[Nsp>=10])
+DENdb=DENdb %>% filter(trait_name=="Skeletal density") %>%
+  mutate(sden=as.numeric(value),genus=substr(specie_name,1,regexpr(pattern = " ",text = specie_name)))
+genmed=DENdb %>% group_by(genus) %>% reframe(sden_med=median(sden)) %>% arrange(desc(sden_med))
+DENdb=DENdb %>% 
+  mutate(genus=factor(genus,levels = genmed$genus))
+
+DENdb.=DENdb %>% filter(specie_name%in%Nsp10,!is.na(latitude))
+
+
+DENdb %>% ggplot(aes(x=specie_name,y=sden))+geom_boxplot()+theme(axis.text.x = element_text(angle=90))+facet_wrap("genus",scales="free_x",drop=T)
+DENdb %>% ggplot(aes(x=genus,y=sden))+geom_boxplot()+theme(axis.text.x = element_text(angle=90))
+DENdb. %>% ggplot(aes(x=sden))+geom_histogram()+facet_wrap("specie_name")
+DENdb. %>% ggplot(aes(x=specie_name,y=sden))+geom_point()
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+DENdb.sp=st_as_sf(DENdb.,coords = c("longitude","latitude"),crs=st_crs(world))
+DENdbsp=st_as_sf(DENdb.,coords = c("longitude","latitude"),crs=st_crs(world))
+
+
+DENdbsp %>% ggplot()+
+  geom_sf(fill = "light gray",data = world) +
+  geom_sf(aes(color=specie_name,size=sden))+
+  scale_size_area()+
+  theme(panel.background = element_rect(fill = "azure"))
+
+
+
+sort(table(DENdb$specie_name))
+
+
+mod1=lm(sden~specie_name+latitude,data=DENdb.)
+
+summary(mod1)
+
+
 ct=load("./Data/ColonyTransitions/HA_Trans/Colony_Data_20210917_edited.rdata")
 ct=ColonyLevel
 ct = ct %>% mutate(LinExt=((EndingSize-StartingSize)/StartingPerim)/Interval_Years)  
